c6acdd2cb9f94956df6811d80d868c23cbeb846a
当无 <2317762441@qq.com>
2023-02-17 19:37:38 +0800

    Added a cache update policy for stores.

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,10 +9,16 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.dto.Result.fail;
+import static com.hmdp.dto.Result.ok;
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -51,13 +57,35 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         Shop shop = getById(id);
         if (shop == null) {
             // 5 数据库中不存在，返回错误
-            return Result.fail("店铺不存在");
+            return fail("店铺不存在");
         }
 
-        // 6 数据库中存在，写入redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        // 6 数据库中存在，写入redis，并添加超时时间30m
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
 
         // 7 返回
         return Result.ok(shop);
+    }
+
+    /**
+     * 确保缓存与数据库中商户信息一致，使用事务
+     * @param shop
+     * @return
+     */
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if (id == null) {
+            return Result.fail("店铺id不能为空");
+        }
+        // 1 更新数据库
+        updateById(shop);
+
+        // 2 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+
+        return Result.ok();
     }
 }
