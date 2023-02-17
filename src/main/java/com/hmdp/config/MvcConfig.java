e8a8b0e997cc92926ba7a66162e668831367eb55
当无 <2317762441@qq.com>
2023-02-17 16:03:28 +0800

    init project

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,40 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 添加拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // token 刷新拦截器
+        registry.addInterceptor(new LoginInterceptor()).
+                excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+
+        // 登录拦截器器
+        // order 越小越先执行
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
+    }
+}
---
+++ src/main/java/com/hmdp/config/WebExceptionAdvice.java
@@ -0,0 +1,21 @@
+package com.hmdp.config;
+
+import com.hmdp.dto.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class WebExceptionAdvice {
+    /**
+     * 通用异常记录日志
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public Result handleRuntimeException(RuntimeException e) {
+        log.error(e.toString(), e);
+        return Result.fail("服务器异常");
+    }
+}
